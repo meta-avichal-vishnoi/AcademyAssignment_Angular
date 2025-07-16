137d8031125ae7dba85847c3febdf6265b30c388
avishnoi2 <avishnoi@idauto.net>
2025-07-16 21:32:14 +0530

    Assignment3: Added a user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from './container/container.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContainerComponent],
+  standalone: true,
+  imports: [RouterOutlet, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-container',
+  standalone: true,
   imports: [],
   templateUrl: './container.component.html',
 })
+
 export class ContainerComponent {
 
 }
